b76a3d6e8d99f14270da59bcf0e6c3fa21bc44a0
yoann <yoann.lh@hotmail.fr>
2020-03-24 10:59:31 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,68 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ESIEA SECURE EDITION 2020
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	<img src={logo} className="App-logo" alt="logo" />
+	    <div class="wrapper">  
+    <div class="container"><ul class="menu" rel="sam1">  
+    <li class="active"><a href="#">Qu'est ce que c'est</a></li>  
+    <li><a href="#">Atelier</a></li>
+    <li><a href="#">Intervenant</a></li>
+    <li><a href="#">Contacts</a></li>
+    </ul>
+    </div>
+    </div>
+
+          <p>
+            ESIEA SECURE EDITION 2020
+          </p>
+          <a
+           className="App-link"
+           href="https://www.esiea.fr/esiea-secure-edition-2017/"
+           target="_blank"
+           rel="noopener noreferrer"
+          >
+            Plus d'info sur le site ESIEA
+          </a>
       </header>
     </div>
   );
 }
 
+
+/*
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Le nom a été soumis : ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+   <div className="NameForm">
+     <form onSubmit={this.handleSubmit}>
+        <label>
+          Nom :
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Envoyer" />
+      </form>
+   </div>
+    );
+  }
+}
+*/
 export default App;
+
+
