66cb22ec60a2fc3facd0bbfd7a8de4e97c087e9d
yoann <yoann.lh@hotmail.fr>
2020-03-24 17:19:56 +0100

    [#1] [move Search.js]

---
+++ src/Components/Search.js
@@ -0,0 +1,18 @@
+// Components/Search.js
+
+import React from 'react'
+import { View, TextInput, Button } from 'react-native'
+
+
+class Search extends React.Component {
+    render() {
+        return (
+      	<View>
+           <TextInput placeholder="Plus d'info ici"/>
+           <Button title='liens' onPress={() => {}}/>
+     	</View>
+        )
+    }
+}
+
+export default Search
