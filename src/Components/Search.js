61ba185d0a067fbc0d04681bb8f5237f2b241fe6
yoann <yoann.lh@hotmail.fr>
2020-03-25 15:23:07 +0100

    [#1][add] update Search.js to add color to the button

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,16 +1,14 @@
 // Components/Search.js
 
 import React from 'react'
-import { TextField, Button } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 
 const Search = () => {
     return (
-        <div>
-	
-           <TextField placeholder="Plus d'info ici"/>
-           <Button title='liens' onPress={() => {}}/>
-	</div>
+     	<Button variant="contained" color="primary">
+      		Hello World
+    	</Button>
         )
    
 }
