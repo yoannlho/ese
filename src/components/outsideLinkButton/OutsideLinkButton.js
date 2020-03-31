e71de0eb825af857b5f368973b540a7ee8294336
yoann <yoann.lh@hotmail.fr>
2020-03-31 16:26:25 +0200

    [#2][update] rename all files and improve code

---
+++ src/components/outsideLinkButton/OutsideLinkButton.js
@@ -0,0 +1,26 @@
+// Components/Search.js
+
+import React from 'react';
+import { Button } from '@material-ui/core';
+import OutsideLinkButtonStyle from './OutsideLinkButton.module.css';
+
+const OutsideLinkButton = (props) => {
+  const { urlLink, labelButton } = props;
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          window.location = urlLink;
+        }}
+        variant="contained" 
+        size="medium"
+        color="primary" 
+        className={ OutsideLinkButtonStyle.margin }
+      >
+        { labelButton }
+      </Button>
+    </div>
+  )      
+}
+
+export default OutsideLinkButton;
