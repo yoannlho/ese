89188df370a8730e342387e89543ca70e2014089
yoann <yoann.lh@hotmail.fr>
2020-04-01 11:09:12 +0200

    [#4][add] update App.js to add switch path

--- src/App.js
+++ src/App.js
@@ -11,35 +11,49 @@ import OutsideLinkButton from './components/outsideLinkButton/OutsideLinkButton'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <div class="wrapper">  
-              <div class="container">
-                <ul class="menu" rel="sam1">  
-                  <li class="active"><Link to="/">Accueil</Link></li>  
-                  <li><Link to="/Atelier">Atelier</Link></li>
-                  <li><Link to="/Intervenant">Intervenant</Link></li>
-                  <li><Link to="/Contacts">Contacts</Link></li>
-                </ul>
-              </div>
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <div class="wrapper">  
+            <div class="container">
+              <ul class="menu" rel="sam1">  
+                <li class="active"><Link to="/">Accueil</Link></li>  
+                <li><Link to="/Atelier">Atelier</Link></li>
+                <li><Link to="/Intervenant">Intervenant</Link></li>
+                <li><Link to="/Contacts">Contacts</Link></li>
+              </ul>
             </div>
-              <p>
-                  ESIEA SECURE EDITION 2020
-              </p>
-              <OutsideLinkButton urlLink="https://www.esiea.fr/esiea-secure-edition-2017/" labelButton="Plus d'info ici"/>
-              {/* <a
-                className="App-link"
-                href="https://www.esiea.fr/esiea-secure-edition-2017/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Plus d'info sur le site ESIEA
-              </a> */}
-          </header>
-      </div>
-    </Router>
+          </div>
+            <p>
+                ESIEA SECURE EDITION 2020
+            </p>
+            <OutsideLinkButton urlLink="https://www.esiea.fr/esiea-secure-edition-2017/" labelButton="Plus d'info ici"/>
+            {/* <a
+              className="App-link"
+              href="https://www.esiea.fr/esiea-secure-edition-2017/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Plus d'info sur le site ESIEA
+            </a> */}
+        </header>
+        <Switch>
+        <Route path="/Atelier">
+          <Atelier />
+        </Route>
+        <Route path="/Intervenant">
+          <Intervenant />
+        </Route>
+        <Route path="/">
+          <Accueil />
+        </Route>
+        <Route path="/Contacts">
+          <Contacts />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
   );
 }
 
