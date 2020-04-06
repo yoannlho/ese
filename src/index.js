80a44b24da87dd9629df8029b73461a146300941
yoann <yoann.lh@hotmail.fr>
2020-04-06 10:58:40 +0200

    [#4][update] add route in the DOM and fix an error in the import file route

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Router } from 'react-router';
+// Your routes.js file
+import routes from './Route/routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Router routes={routes}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
