8ffb973ac708f01305f26270b136180619c8efc0
yoann <yoann.lh@hotmail.fr>
2020-04-07 10:11:44 +0200

    [#4][update] code update

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
   <Router>
     <div className="App">
-      <routes />
+    <routes />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
           <div class="wrapper">  
