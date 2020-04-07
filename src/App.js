f3e17e006f1c82eaf5482aed116b1296cd5f730a
yoann <yoann.lh@hotmail.fr>
2020-04-07 14:36:43 +0200

    [#4][update] add logo in App.js in tag header

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Error404 from './components/Error404';
 import Landing from './components/Landing';
-//import logo from './picture/ese.svg';
+import logo from './picture/ese.svg';
 //import './App.css';
 //import OutsideLinkButton from './components/outsideLinkButton/OutsideLinkButton';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Landing }/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Landing }/>
 
-        <Route component={ Error404 }/>
-      </Switch>
-    </BrowserRouter>
+            <Route component={ Error404 }/>
+          </Switch>
+        </BrowserRouter>
+      </header>
+    </div>
   );
 }
 
