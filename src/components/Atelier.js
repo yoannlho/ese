994e69acdfa9fe7dea9d508234816747d25d1c8d
yoann <yoann.lh@hotmail.fr>
2020-04-07 09:42:05 +0200

    [#4][fix] error is due to the previous comment so I delete them

--- src/components/Atelier.js
+++ src/components/Atelier.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from '../picture/ese.svg';
 import '../App.css';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   Link
+ } from "react-router-dom";
 //import routes from './Route/routes';
 // import Atelier from './components/Atelier';
 // import Intervenant from './components/Intervenant';
