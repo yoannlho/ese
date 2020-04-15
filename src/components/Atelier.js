136332b6685fc8c25217a83a85078849fe4b0305
yoann <yoann.lh@hotmail.fr>
2020-04-15 15:39:44 +0200

    [#5][update] update Atelier.js for test

--- src/components/Atelier.js
+++ src/components/Atelier.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import logo from '../picture/ese.svg';
 import '../App.css';
+import { Link } from "react-router-dom";
 
+const Atelier = () => {
+  return (
+    <div>toto</div>
+  )
+}
+
+export default Atelier;
 
