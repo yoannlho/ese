92b3eadb1ea0554b866325697e930d4411374666
yoann <yoann.lh@hotmail.fr>
2020-04-20 15:06:21 +0200

    [#7][Add] update Contact.js by adding header tag

---
+++ src/components/Contacts.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../picture/ese.svg';
+import '../App.css';
+import { Link } from "react-router-dom";
+
+const Contacts = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                    <div class="wrapper">  
+                        <div class="container">
+                            <ul class="menu" rel="sam1">  
+                                <li class="active"><Link to="/">Accueil</Link></li>  
+                                <li><Link to="/Atelier">Atelier</Link></li>
+                                <li><Link to="/Intervenant">Intervenant</Link></li>
+                                <li><Link to="/Contacts">Contacts</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <p> 
+                        Contacts 
+                    </p>
+            </header>
+        </div>
+    )
+}
