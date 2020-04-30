7ad68163d23caf0dc3c4f69df3d9fa03f8be08ca
yoann <yoann.lh@hotmail.fr>
2020-04-30 11:49:39 +0200

    [#5][update] update NameForm.js with the right label

--- src/components/formControl/NameForm.js
+++ src/components/formControl/NameForm.js
@@ -16,9 +16,8 @@ const BasicTextFields = () => {
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="standard-basic" label="Standard" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="standard-basic" label="Nom" />
+      <TextField id="filled-basic" label="Prénom" />
     </form>
   );
 }
