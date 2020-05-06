7f1ac22e6af011e94a9b09a635e7431bd5f4e9e1
yoann <yoann.lh@hotmail.fr>
2020-05-06 16:38:10 +0200

    [#6][update] update Intervenant by delete the useless header

--- src/components/Intervenant.js
+++ src/components/Intervenant.js
@@ -7,23 +7,11 @@ import AlignItemsList from './listItem/AlignItemsList';
 const Intervenant = () => {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                    <div class="wrapper">  
-                        <div class="container">
-                            <ul class="menu" rel="sam1">  
-                                <li class="active"><Link to="/">Accueil</Link></li>  
-                                <li><Link to="/Atelier">Atelier</Link></li>
-                                <li><Link to="/Intervenant">Intervenant</Link></li>
-                                <li><Link to="/Contacts">Contacts</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <h3> 
-                        Intervenant 
-                    </h3>
-                    <AlignItemsList/>
-            </header>
+            <h3> 
+                Intervenant 
+            </h3>
+            <AlignItemsList/>
+
         </div>
     )
 }
