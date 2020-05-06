4e579708f69e098582c9edc3f4ced9bebd36d25a
yoann <yoann.lh@hotmail.fr>
2020-05-06 16:35:52 +0200

    [#5][update] update atelier to make a new form

--- src/components/Atelier.js
+++ src/components/Atelier.js
@@ -1,61 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../picture/ese.svg';
 //import '../App.css';
 import { Link } from "react-router-dom";
 import FormControl from '@material-ui/core/FormControl';
-import { InputLabel, FormHelperText, Input } from '@material-ui/core';
+import { InputLabel, FormHelperText, Input, TextField, Button } from '@material-ui/core';
 import RadioButtonsGroup from './formControl/RadioGroup';
 import BasicTextFields from './formControl/NameForm';
 import ContainedButtons from './formControl/Button';
 import './filecss/AtelierStyle.css';
 
 const Atelier = () => {
+  const [firstName, setFirstName] = useState() 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    alert(`firstName: ${firstName}`)
+  };
+  const changeHandler = (event) => {
+    setFirstName(event.target.value)
+  }
   return (
     <div className="App">
-            <header className="App-header">
-                <img src={ logo } className="App-logo" alt="logo" />
-                <div class="wrapper">  
-                    <div class="container">
-                        <ul class="menu" rel="sam1">  
-                            <li class="active"><Link to="/">Accueil</Link></li>  
-                            <li><Link to="/Atelier">Atelier</Link></li>
-                            <li><Link to="/Intervenant">Intervenant</Link></li>
-                            <li><Link to="/Contacts">Contacts</Link></li>
-                        </ul>
-                    </div>
-                </div>
-                  <span>
-                    Inscription                  
-                  </span>
-                  <p>
-                    <BasicTextFields labelname="Nom" labelfirstname="Prénom"/>
-                  </p>
-                  <p>
-                    <FormControl>
-                      <InputLabel htmlFor="my-input">Email address</InputLabel>
-                      <Input id="my-input" aria-describedby="my-helper-text" />
-                      <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-                    </FormControl>
-                  </p>
-                  <p>
-                    <RadioButtonsGroup/>
-                  </p>
-                  <p>
-                    <ContainedButtons/>
-                  </p>
-                  <p>
-                    Atelier :                   
-                  </p>
-                  <p>
-                    1) Workshop sur CyberRange (CTF guidé avec présentation des outils)
-                  </p>
-                  <p>
-                    2) Démo OSINT
-                  </p>
-                  <p>
-                    Il est prévu que ces deux ateliers se déroulent en ligne via la plateforme de vision conférence jitsy.
-                  </p>
-            </header>
+      <span>
+        Inscription                  
+      </span>
+      <form onSubmit={ submitHandler }>
+        <TextField 
+          id="first_name"
+          label="First Name"
+          value={ firstName }
+          onChange={ changeHandler }        
+        />
+        <Button 
+          type="submit"
+          label="envoyer"
+        
+        />
+      </form>
     </div>
   )
 }
