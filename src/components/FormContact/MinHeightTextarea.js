bfe21476194dc1b213089f360fe6b9963fa79676
yoann <yoann.lh@hotmail.fr>
2020-05-07 10:40:54 +0200

    [#7][update] update component MinHeightTextArea to clean useless import and make a correct indentation

--- src/components/FormContact/MinHeightTextarea.js
+++ src/components/FormContact/MinHeightTextarea.js
@@ -2,12 +2,15 @@ import React from 'react';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
 const MinHeightTextarea = (props) => {
-    const {label, rowsMin, placeholder} = props;
-        return <TextareaAutosize 
-            aria-label={ label } 
-            rowsMin={ rowsMin } 
-            placeholder={ placeholder } 
-        />;
+  const {label, rowsMin, placeholder} = props;
+  
+    return (
+      <TextareaAutosize 
+        aria-label={ label } 
+        rowsMin={ rowsMin } 
+        placeholder={ placeholder } 
+      />
+    )
 }
 
 export default MinHeightTextarea;
