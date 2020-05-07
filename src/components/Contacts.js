94f8004dc99ab6fe444e56230431f05121a05bcb
yoann <yoann.lh@hotmail.fr>
2020-05-07 10:41:43 +0200

    [#7][update] update contact.js to clean useless import and make a correct indentation

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import logo from '../picture/ese.svg';
 import '../App.css';
-import { Link } from "react-router-dom";
 import MinHeightTextarea from './FormContact/MinHeightTextarea';
 
 const Contacts = () => {
-    return (
-        <div className="App">
-                    <h3> 
-                        Contacts 
-                    </h3>
-                    <MinHeightTextarea label="minimum height" rowsMin={3} placeholder="Minimum 3 rows"/>
-        </div>
-    )
+  return (
+    <div className="App">
+      <h3> 
+          Contacts 
+      </h3>
+      <MinHeightTextarea label="minimum height" rowsMin={3} placeholder="Minimum 3 rows"/>
+    </div>
+  )
 }
 
 export default Contacts;
