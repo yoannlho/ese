f006156c885336acdbc7a0393f019b45ebc49a98
yoann <yoann.lh@hotmail.fr>
2020-05-07 10:39:28 +0200

    [#6][update] update Itervenant to clean useless import and make a correct indentation

--- src/components/Intervenant.js
+++ src/components/Intervenant.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import logo from '../picture/ese.svg';
 import '../App.css';
-import { Link } from "react-router-dom";
 import AlignItemsList from './listItem/AlignItemsList';
 
 const Intervenant = () => {
-    return (
-        <div className="App">
-            <h3> 
-                Intervenant 
-            </h3>
-            <AlignItemsList/>
-
-        </div>
-    )
+  return (
+    <div className="App">
+      <h3> 
+        Intervenant 
+      </h3>
+      <AlignItemsList/>
+    </div>
+  )
 }
 
 export default Intervenant;
