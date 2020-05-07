6546aa680a6dc4551141456bf737d75e0bd8f5e2
yoann <yoann.lh@hotmail.fr>
2020-05-07 10:34:50 +0200

    [#4][update] update Landing.js to clean useless import and make a correct indentation

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 import OutsideLinkButton from './outsideLinkButton/OutsideLinkButton';
 
 const Landing = () => {
-    return (
-        <div className="App">
-                <h3>
-                    ESIEA SECURE EDITION 2020
-                </h3>
-                <OutsideLinkButton urlLink="https://www.esiea.fr/esiea-secure-edition-2017/" labelButton="Plus d'info ici"/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h3>
+          ESIEA SECURE EDITION 2020
+      </h3>
+      <OutsideLinkButton urlLink="https://www.esiea.fr/esiea-secure-edition-2017/" labelButton="Plus d'info ici"/>
+    </div>
+  );
 }
 
 export default Landing;
