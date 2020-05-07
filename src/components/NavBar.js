0d2d8abb2b80c669938af96a0b9d7ca27b949985
yoann <yoann.lh@hotmail.fr>
2020-05-07 15:14:48 +0200

    [#4][update] update Navbar.js to add logo ESE in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import logo from '../picture/ese.svg';
 //import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
@@ -27,7 +28,7 @@ const NavBar = () => {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            {/* <MenuIcon /> */}
+            <img src={ logo } className="App-logo" alt="logo" />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             News
