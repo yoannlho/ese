b90fbb97d43b5938b8735dc546f0839a914706c6
yoann <yoann.lh@hotmail.fr>
2020-05-26 16:35:25 +0200

    [#4][update] Change position of the tag logo

--- src/components/navBar/NavBardyn.js
+++ src/components/navBar/NavBardyn.js
@@ -2,18 +2,18 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import logo from '../../picture/ese.svg';
 import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import NavStyle from './NavBardyn.module.css';
+import logo from '../../picture/ese.svg';
 
 const NavBardyn = () => {
   return (
     <div className={ NavStyle.root }>
       <AppBar position="static">
+      <img src={ logo } className="App-logo" alt="logo" />
         <Toolbar>
           <IconButton edge="start" className={ NavStyle.menuButton } color="inherit" aria-label="menu">
-            <img src={ logo } className="App-logo" alt="logo" />
           </IconButton>
           <Link to="/">
             <Button>Accueil</Button> 
