1370af178c0f195d2f7170d7a9ff74d3b229b890
yoann <yoann.lh@hotmail.fr>
2020-05-26 16:06:13 +0200

    [#4][add] Add NavBardyn to a new location and add NavBardyn.module.css

---
+++ src/components/navBar/NavBardyn.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import logo from '../../picture/ese.svg';
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import NavStyle from './NavBardyn.module.css';
+
+const NavBardyn = () => {
+  return (
+    <div className={ NavStyle.root }>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={ NavStyle.menuButton } color="inherit" aria-label="menu">
+            <img src={ logo } className="App-logo" alt="logo" />
+          </IconButton>
+          <Link to="/">
+            <Button>Accueil</Button> 
+          </Link>
+
+          <Link to="/Atelier">
+            <Button>Atelier</Button>
+          </Link>
+
+          <Link to="/Intervenant" color="body2">Intervenant</Link>
+          <Link to="/Contacts" color="body2">Contacts</Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default NavBardyn;
