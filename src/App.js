07372596e0a490c28d36408f73cb8391a21a43db
yoann <yoann.lh@hotmail.fr>
2020-05-27 10:40:49 +0200

    [#4][update] update App.js by cleaning useless import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Error404 from './components/Error404';
 import Landing from './components/Landing';
 import Atelier from './components/Atelier';
