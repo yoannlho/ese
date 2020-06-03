74ba14a119b2d0d278547b1a38406b5ba8bf48a0
yoann <yoann.lh@hotmail.fr>
2020-06-03 17:13:50 +0200

    [#6][update] add import css file in file.js

--- src/components/listItem/AlignItemsList.js
+++ src/components/listItem/AlignItemsList.js
@@ -6,6 +6,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import AlignItem from './AlignItemsList.module.css';
 
 const AlignItemsList = () => {
   return (
