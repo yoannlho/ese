5ccccaa0bb5642eacd7c732458869d765b13e3a6
yoann <yoann.lh@hotmail.fr>
2020-06-08 16:04:35 +0200

    [#6][update] add className and import css file to consider the modification of the title

--- src/components/Intervenant.js
+++ src/components/Intervenant.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AlignItemsList from './listItem/AlignItemsList';
+import './filecss/IntervenantStyle.css';
 
 const Intervenant = () => {
   return (
     <div className="App">
-      <h3> 
+      <h3 className="title"> 
         Intervenant 
       </h3>
       <AlignItemsList/>
