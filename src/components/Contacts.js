881cb29fde3a0fb3e60e1849e9437516b6c0d6a5
yoann <yoann.lh@hotmail.fr>
2020-07-02 15:20:41 +0200

    [#18][update] comment useless code that is not used anymore

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import MinHeightTextarea from './FormContact/MinHeightTextarea';
+//import MinHeightTextarea from './FormContact/MinHeightTextarea';
 import './filecss/ContactStyle.css';
 import { TextField, Button } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
