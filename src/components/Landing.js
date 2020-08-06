b95378edbe91ce9126994044a6fde3705b0da814
yoann <yoann.lh@hotmail.fr>
2020-08-06 16:42:05 +0200

    [#32][Add] add import file css

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import OutsideLinkButton from './outsideLinkButton/OutsideLinkButton';
+import './filecss/LandingStyle.css';
 
 const Landing = () => {
   return (
     <div className="App">
-      <h3>
+      <h3 className="position_title_landing">
           ESIEA SECURE EDITION 2020
       </h3>
       <OutsideLinkButton urlLink="https://www.esiea.fr/esiea-secure-edition-2017/" labelButton="Plus d'info ici"/>
