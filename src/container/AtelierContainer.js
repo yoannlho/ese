6b77314ab9c8d9680d278f28c9b5eca0be9336d5
yoann <yoann.lh@hotmail.fr>
2020-08-12 15:34:24 +0200

    [#34][add] add state in container

---
+++ src/container/AtelierContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class AtelierContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        telephone: '',
+        gender: '',
+        studentcard: '',
+        textarea: ''
+      },
+}
