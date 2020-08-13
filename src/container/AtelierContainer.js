3aba4e18e7bec3f72e40afb346aef2986ea6e62c
yoann <yoann.lh@hotmail.fr>
2020-08-13 10:25:07 +0200

    [#34][update] add import Icon for button from material UI

--- src/container/AtelierContainer.js
+++ src/container/AtelierContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { TextField, Button, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio} from '@material-ui/core';
 import './filecss/AtelierStyle.css';
+import SendIcon from '@material-ui/icons/Send';
 
 class AtelierContainer extends Component {
   constructor(props) {
