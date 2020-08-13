d8ec7a661a1feb9d643d96b2eb30662bcb695ddc
yoann <yoann.lh@hotmail.fr>
2020-08-13 16:43:21 +0200

    [#34][update] delete useless code that doesn't show up

--- src/components/Atelier.js
+++ src/components/Atelier.js
@@ -3,10 +3,9 @@ import './filecss/AtelierStyle.css';
 import AtelierContainer from '../container/AtelierContainer';
 
 const Atelier = () => {
-  
+
   return (
     <div className="App">
-      <h3>React Form</h3>
       <AtelierContainer />
     </div>
   )
