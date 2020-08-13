c3a998baa05c661d34ddd21c5c4b27c4219a3a15
yoann <yoann.lh@hotmail.fr>
2020-08-13 15:12:09 +0200

    [#34][update] delete useless code that is not used anymore by adding container

--- src/components/Atelier.js
+++ src/components/Atelier.js
@@ -1,131 +1,13 @@
-import React, { useState } from 'react';
-import { TextField, Button, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio} from '@material-ui/core';
+import React from 'react';
 import './filecss/AtelierStyle.css';
-import SendIcon from '@material-ui/icons/Send';
+import AtelierContainer from '../container/AtelierContainer';
 
 const Atelier = () => {
-  const [firstName, setFirstName] = useState()
-  const [lastName, setLastName] = useState()
-  const [email, setEmail] = useState()
-  const [telephone, setTelephone] = useState()
-  const [gender, setGender] = useState('Male')
-  const [studentcard, setStudentcard] = useState()
-  const [textarea, setTextArea] = useState()
-  const submitHandler = (event) => {
-    event.preventDefault();
-    var result = Array(`firstName: ${firstName}, lastName: ${lastName}, email: ${email}, telephone: ${telephone}, studentcard: ${studentcard}, genre: ${gender}, Textarea: ${textarea}`)
-    document.getElementById("text").innerHTML = result
-  };
-  const changeHandlerFirstName = (event) => {
-    setFirstName(event.target.value)
-  }
-  const changeHandlerLastName = (event) => {
-    setLastName(event.target.value)
-  }
-  const changeHandlerEmail = (event) => {
-    setEmail(event.target.value)
-  }
-  const changeHandlerTelephone = (event) => {
-    setTelephone(event.target.value)
-  }
-  const changeHandlerGender = (event) => {
-    setGender(event.target.value)
-  }
-  const changeHandlerStudentcard = (event) => {
-    setStudentcard(event.target.value)
-  }
-  const changeHandlerTextarea = (event) => {
-    setTextArea(event.target.value)
-  }
-
+  
   return (
     <div className="App">
-      <form className="flex-container" onSubmit={ submitHandler }>
-      <h3>
-        Inscription
-      </h3>
-      <h6>Les champs obligatoires sont précédés d'une étoile *</h6>
-        <TextField className="filled_buttonfirst"
-          id="first_name"
-          label="Prénom"
-          variant="filled"
-          color="secondary" 
-          required
-          type="text"
-          value={ firstName }
-          onChange={ changeHandlerFirstName }      
-        />
-          <span id="text"></span>
-        <TextField className="filled_buttonlast"
-          id="last_name"
-          label="Nom"
-          variant="filled"
-          color="secondary"
-          required
-          type="text"
-          value={ lastName }
-          onChange={ changeHandlerLastName }      
-        />
-        <TextField className="filled_buttonemail"
-          id="email"
-          label="Adresse Email"
-          variant="filled"
-          color="secondary" 
-          required
-          type="email"
-          placeholder="mail@serveur.com"
-          value={ email }
-          onChange={ changeHandlerEmail }      
-        />
-        <TextField className="filled_buttontelephone"
-          id="telephone"
-          label="téléphone"
-          variant="filled"
-          color="secondary"
-          type="tel"
-          pattern="[0-9]{10}"
-          value={ telephone }
-          onChange={ changeHandlerTelephone }  
-        />
-        <TextField className="filled_buttonstudentcard"
-          id="student_card"
-          label="Numéro de carte d'étudiant"
-          variant="filled"
-          color="secondary"
-          type="text"
-          value={ studentcard }
-          onChange={ changeHandlerStudentcard }  
-        />
-        <FormControl component="fieldset" className="gender_position">
-          <FormLabel component="legend" className="title_gender">Genre</FormLabel>
-          <RadioGroup aria-label="gender" name="gender1" value={ gender } onChange={ changeHandlerGender }>
-            <FormControlLabel value="male" control={<Radio />} label="Homme" />
-            <FormControlLabel value="female" control={<Radio />} label="Femme" />
-          </RadioGroup>
-        </FormControl>
-        <TextField className="form_position2"
-        label="Suggestions d'idées d'atelier"
-        multiline
-        rowsMax={ 10 }
-        rows= { 10 }
-        variant="filled"
-        color="secondary"
-        value={ textarea } 
-        onChange={ changeHandlerTextarea }
-        />
-        <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            className="button_position"
-            //label="envoyer"
-            endIcon={<SendIcon />}
-         
-            >
-                Envoyer
-          
-          </Button>
-      </form>
+      <h3>React Form</h3>
+      <AtelierContainer />
     </div>
   )
 }
