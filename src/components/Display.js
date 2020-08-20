22bf5a2938bb5d598afb68a6b230c13536f41453
yoann <yoann.lh@hotmail.fr>
2020-08-20 16:55:22 +0200

    [#35][update] add title to the display page

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Display = () => {
-  return ()
+  return (
+    <div className="App">
+      <h3>
+          Display input user
+      </h3>
+    </div>
+  )
 }
 
 export default Display;
